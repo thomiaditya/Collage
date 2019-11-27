4735e876943c1d3b30e5d2ae0b081c97f7e6d5cf
thomiaditya <thomiaditya@gmail.com>
2019-11-27 14:16:35 +0700

    Create Mini Max Hackerrank.cpp

---
+++ Mini Max Hackerrank.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+	int arr[5], min, max;
+	
+	for(int i = 0; i < 5; i++) {
+		cin >> arr[i];
+	}
+	
+	for(int i = 0; i < 5; i++) {
+		int sum = 0;
+		for(int j = 0; j < 5; j++) {
+			if(i != j) {
+				sum += arr[j];
+			}
+		}
+		if(i == 0) {
+			max = sum;
+			min = sum;
+		}
+		
+		if(sum > max) max = sum;
+		if(sum < min) min = sum;
+	}
+	
+	cout << min << " " << max;
+}
