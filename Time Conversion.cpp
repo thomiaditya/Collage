c88788160831c5ffff17f0982f53767e832f68c0
thomiaditya <thomiaditya@gmail.com>
2019-11-27 15:34:50 +0700

    Update Time Conversion.cpp

--- Time Conversion.cpp
+++ Time Conversion.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 
 void clock(string str) {
-	
-	int hh = (((int)str[0] - 48) * 10) + ((int)str[1] - 48);
-	
-	if(str[8] == 'P') {
-		hh = hh + 12 == 24 ? 0: hh + 12;
-	}
-	if(hh == 0) cout << '0' << hh;
-	else cout << hh;
-	for(int i = 2; i < 8; i++) {
-		cout << str[i];
-	}
+    
+    int hh = (((int)str[0] - 48) * 10) + ((int)str[1] - 48);
+    
+    if(str[8] == 'P' && hh < 12) {
+        hh += 12;
+    } else if(hh == 12 && str[8] == 'A') {
+        hh = 0;
+    }
+    if(hh == 0) cout << '0' << hh;
+    else if(hh / 10 < 1) cout << '0' << hh;
+    else cout << hh;
+    for(int i = 2; i < 8; i++) {
+        cout << str[i];
+    }
 }
 
 int main() {
-	string ans;
-	cin >> ans;
-	
-	
-	clock(ans);
+    string ans;
+    cin >> ans;
+    clock(ans);
 }
