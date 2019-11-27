5f310f9f35e164250f328aa67e93500f7aa47c4b
thomiaditya <thomiaditya@gmail.com>
2019-11-27 14:48:22 +0700

    Create Time Conversion.cpp

---
+++ Time Conversion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void clock(string str) {
+	
+	int hh = (((int)str[0] - 48) * 10) + ((int)str[1] - 48);
+	
+	if(str[8] == 'P') {
+		hh = hh + 12 == 24 ? 0: hh + 12;
+	}
+	if(hh == 0) cout << '0' << hh;
+	else cout << hh;
+	for(int i = 2; i < 8; i++) {
+		cout << str[i];
+	}
+}
+
+int main() {
+	string ans = "12:05:45PM";
+	
+	cout << ans << endl;
+	
+	clock(ans);
+}
